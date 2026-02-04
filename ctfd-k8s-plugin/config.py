eef55597d535190c6484c872e9fb2cd787ac1c66
ferrodr <fernando.rguezm@gmail.com>
2026-02-04 22:34:27 +0100

    Add API host internal for URL

--- ctfd-k8s-plugin/config.py
+++ ctfd-k8s-plugin/config.py
@@ -2,3 +2,4 @@ import os
 
 K8S_API_URL = os.getenv('K8S_CTF_API_URL', 'http://localhost:5000')
 K8S_API_HOST = os.getenv('K8S_CTF_API_HOST', 'localhost')
+K8S_API_HOST_INTERNAL = os.getenv('K8S_CTF_API_HOST', 'k8s-api.lan')
